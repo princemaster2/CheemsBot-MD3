e732192cf827e9640379c2ba5a42583fd19714b9
princemaster2 <110698769+princemaster2@users.noreply.github.com>
2022-09-11 07:22:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -92,7 +92,7 @@ global.ntilink = []
 global.lolhuman = "KaysaS"
 global.sessionName = "session"
 global.antitags = true
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','🐦','🐤',']
 global.mess = {
     success: 'Done✓ Thanks To Prince 🤭',
     admin: 'This feature is only for admins 𝘽𝘼𝙆𝘼!',
